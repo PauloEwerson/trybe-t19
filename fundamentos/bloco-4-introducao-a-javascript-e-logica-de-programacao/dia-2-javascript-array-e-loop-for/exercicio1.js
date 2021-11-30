9c1d3fbc7853ec87c7d2a6e93908147f54ac1363
Ewerson Martins <ewersonmrt@gmail.com>
2021-11-30 02:03:43 -0300

    resolve exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio1.js
@@ -1,8 +1,11 @@
-/* percorra o array imprimindo todos os valores nele contidos 
+/* Percorra o array imprimindo todos os valores nele contidos 
 com a função console.log(); */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let soma = 0;
 for (let i = 0; i < numbers.length; i += 1) {
-  console.log(numbers[i]);
+  soma += numbers[i];
 }
+
+console.log(soma);
