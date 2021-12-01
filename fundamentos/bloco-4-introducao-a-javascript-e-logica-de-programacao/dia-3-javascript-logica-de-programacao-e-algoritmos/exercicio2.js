8fc30519c50022b804395313861fe9321b64418f
Ewerson Martins <ewersonmrt@gmail.com>
2021-12-01 02:14:34 -0300

    resolve exercicio 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -0,0 +1,9 @@
+/* Imprima um triângulo retângulo com 5 asteriscos de base. */
+
+let n = 5;
+let asteriscos = '';
+
+for (let i = 0; i < n; i += 1) {
+  asteriscos += '*';
+  console.log(asteriscos);
+}
