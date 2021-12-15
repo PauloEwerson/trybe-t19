d2da7503f3a278b1a3c1d37255b062afbb3fcbe6
Ewerson Martins <ewersonmrt@gmail.com>
2021-12-15 17:24:34 -0300

    06 - cria funcoes com efeito de zoom sob os dias

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -104,3 +104,25 @@ function displayFridays(fridaysArray) {
 
 let dezFridays = [ 4, 11, 18, 25 ];
 displayFridays(dezFridays);
+
+// REQUESITO 06
+function dayMouseOver() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600';
+  })
+};
+
+function dayMouseOut() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseout', function(event) {
+    event.target.style.fontWeight = '200';
+    event.target.style.fontSize = '20px';
+  })
+};
+
+dayMouseOver();
+dayMouseOut();
