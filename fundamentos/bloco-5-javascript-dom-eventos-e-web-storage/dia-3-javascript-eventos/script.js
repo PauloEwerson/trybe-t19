7b624d5858a0b0f96ea6d0be5276de6d338af25c
Ewerson Martins <ewersonmrt@gmail.com>
2021-12-15 16:50:18 -0300

    05 - muda os dias nas sextas-feiras para outro texto com evento click

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -84,3 +84,23 @@ function buttonFriday(buttonName) {
   getDivButtonsContainer.appendChild(buttonFriday); // atribui o botão criado (buttonFriday) como filho da div (getDivButtonsContainer).
 };
 buttonFriday('Sexta-feira');
+
+// REQUESITO 05
+function displayFridays(fridaysArray) {
+  let getFridayButton = document.querySelector('#btn-friday');
+  let fridays = document.getElementsByClassName('friday');
+  let newFridayText = 'SEXTOU o/';
+
+  getFridayButton.addEventListener('click', function() {
+  for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerHTML !== newFridayText) {
+        fridays[index].innerHTML = newFridayText;
+    } else {
+        fridays[index].innerHTML = fridaysArray[index];
+      }
+    }
+  })
+};
+
+let dezFridays = [ 4, 11, 18, 25 ];
+displayFridays(dezFridays);
